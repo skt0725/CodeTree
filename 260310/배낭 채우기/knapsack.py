311303adeac3199d1594793e95cda13020f05bc0
skt0725 <86391886+skt0725@users.noreply.github.com>
2026-03-10 15:07:41 +0900

    Title: 배낭 채우기, Time: 95ms, Memory: 25MB, Status: Passed - Codetree

--- 260310/배낭 채우기/knapsack.py
+++ 260310/배낭 채우기/knapsack.py
@@ -2,15 +2,13 @@ n, m = tuple(map(int, input().split()))
 jewels = [tuple(map(int, input().split())) for _ in range(n)]
 jewels.sort()
 
-dp = [[0] * (m+1) for _ in range(n)]
-answer = 0
+dp = [[0] * (m+1) for _ in range(n+1)]
 for i in range(n):
     for j in range(m+1):
         if jewels[i][0] > j:
-            dp[i][j] = dp[i-1][j]
+            dp[i+1][j] = dp[i][j]
         else:
-            dp[i][j] = max(dp[i-1][j-jewels[i][0]] + jewels[i][1], dp[i-1][j])
-            answer = max(answer, dp[i][j])
+            dp[i+1][j] = max(dp[i][j-jewels[i][0]] + jewels[i][1], dp[i][j])
 print(dp[-1][-1])
 
         
