b2f87cab181e4539b009ef897a425835c38fd321
skt0725 <86391886+skt0725@users.noreply.github.com>
2026-03-26 20:57:02 +0900

    Title: 정수 명령어, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260326/정수 명령어/Integer-command.py
@@ -0,0 +1,22 @@
+T = int(input())
+from sortedcontainers import SortedSet
+
+s = SortedSet()
+
+for _ in range(T):
+    K = int(input())
+    for _ in range(K):
+        oper, num = tuple(input().split())
+        if oper == 'I':
+            s.add(int(num))
+        else:
+            if not s: continue
+            if int(num) == 1:
+                s.remove(s[-1])
+            else:
+                s.remove(s[0])
+    if not s:
+        print('EMPTY')
+    else:
+        print(s[-1], s[0])
+        
