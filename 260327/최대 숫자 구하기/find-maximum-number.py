065783491432ce877a606cf81186720c172a100c
skt0725 <86391886+skt0725@users.noreply.github.com>
2026-03-27 15:08:31 +0900

    Title: 최대 숫자 구하기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260327/최대 숫자 구하기/find-maximum-number.py
@@ -0,0 +1,8 @@
+n, m = tuple(map(int, input().split()))
+from sortedcontainers import SortedSet
+balls = SortedSet(list(range(1, m+1)))
+nums = list(map(int, input().split()))
+
+for num in nums:
+    balls.remove(num)
+    print(balls[])
