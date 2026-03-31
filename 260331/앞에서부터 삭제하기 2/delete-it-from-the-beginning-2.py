8f9e6464f5f0980d04110c0a73b376a08763cdcf
skt0725 <86391886+skt0725@users.noreply.github.com>
2026-03-31 15:05:16 +0900

    Title: 앞에서부터 삭제하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260331/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 260331/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -3,8 +3,10 @@ nums = list(map(int, input().split()))
 import heapq
 answer = 0
 arr = [nums[n-1]]
+acc_sum = nums[n-1]
 for k in range(n-2, 0, -1):
     heapq.heappush(arr, nums[k])
-    avg = (sum(arr)-arr[0])/(len(arr)-1)
+    acc_sum += nums[k]
+    avg = (acc_sum-arr[0])/(len(arr)-1)
     answer = max(answer, avg)
 print(f'{answer:.2f}')
