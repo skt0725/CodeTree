cb1772d42f5837e084eeaa0815fbf5191f7eb64e
skt0725 <86391886+skt0725@users.noreply.github.com>
2026-04-05 13:38:19 +0900

    Title: 1차원 바람, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260405/1차원 바람/The-1D-wind-blows.py
@@ -0,0 +1,41 @@
+n, m, q = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+winds = [(int(r), d) for r, d in [input().split() for _ in range(q)]]
+
+# Please write your code here.
+def check_propagate(row1, row2):
+    for i in range(m):
+        if a[row1][i] == a[row2][i]:
+            return True
+    return False
+
+def shift(index, direc):
+    if direc == 'R':
+        temp = a[index][0]
+        for i in range(m-1):
+            a[index][i] = a[index][i+1]
+        a[index][-1] = temp
+    else:
+        temp = a[index][-1]
+        for i in range(m-1, 0, -1):
+            a[index][i] = a[index][i-1]
+        a[index][0] = temp
+
+
+for wind in winds:
+    row, direc = wind
+    row = int(row)-1
+    shift(row, direc)
+    for i in range(row-1, -1, -1):
+        if direc == 'R': direc = 'L'
+        else: direc = 'R'
+        if check_propagate(i, i+1):
+            shift(i, direc)
+    for j in range(row+1, n):
+        if direc == 'R': direc = 'L'
+        else: direc = 'R'
+        if check_propagate(j, j-1):
+            shift(j, direc)
+
+for row in a:
+    print(*row)
