b7fceaed95936eb26904827f2bc42471b9d0be4b
skt0725 <86391886+skt0725@users.noreply.github.com>
2026-04-09 16:33:22 +0900

    Title: 대폭발, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260409/대폭발/big-explosion.py
@@ -0,0 +1,25 @@
+n, m, r, c = map(int, input().split())
+
+# Please write your code here.
+r -= 1
+c -= 1
+direcs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+bombs = {(r, c)}
+def inrange(r, c):
+    if 0 <= r < n and 0 <= c < n:
+        return True
+    return False
+for t in range(1, m+1):
+    temp = set()
+    for bomb in bombs:
+        r_, c_ = bomb
+        for direc in direcs:
+            n_r, n_c = r_ + direc[0]*t, c_ + direc[1]*t
+            if inrange(n_r, n_c):
+                temp.add((n_r, n_c))
+    bombs |= temp
+print(len(bombs))
+
+
+
+        
